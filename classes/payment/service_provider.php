c8a515c82104c5e47f9a0c91925cf7dd25dfea1d
Snickser <snickser@gmail.com>
2025-03-18 09:58:57 +0300

    add

--- classes/payment/service_provider.php
+++ classes/payment/service_provider.php
@@ -92,8 +92,13 @@ class service_provider implements \core_payment\local\callback\service_provider 
     public static function get_success_url(string $paymentarea, int $instanceid): \moodle_url {
         global $DB;
 
-        $courseid = $DB->get_field('gwpayments', 'course', ['id' => $instanceid], MUST_EXIST);
-        return new \moodle_url('/course/view.php', ['id' => $courseid]);
+        $data = $DB->get_record('gwpayments', ['id' => $instanceid], '*', MUST_EXIST);
+
+	if (isset($data->coursemodule) && $data->coursemodule && $data->studentdisplayonpayments || is_siteadmin()) {
+            return new \moodle_url('/mod/gwpayments/view.php', ['id' => $data->coursemodule]);
+	} else {
+            return new \moodle_url('/course/view.php', ['id' => $data->course]);
+        }
     }
 
     /**
